2389209a053c8d74a2735ebdf2b80d7614a59d4b
Adhithyarajan <adhiadhithya39@gmail.com>
2022-05-06 16:51:30 +0530

    first commit

---
+++ ams.sh
@@ -0,0 +1,17 @@
+echo "enter the digit"
+read n
+b=$n
+s=0
+while [ $n -gt 0 ]
+do
+r=`expr $n % 10`
+s=`expr $s + $r \* $r \* $r`
+n=`expr $n / 10`
+done
+if [ $s -eq $b ]
+then
+echo "amstrong"
+else
+echo "not amstrong"
+fi
+
